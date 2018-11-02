00f46419f7073cd8c642abcea89176b2d4a80540
lschierbach <lschierbach@gmail.com>
2018-11-02 12:59:07 +0100

    Force: added big five

--- include/game/force.hpp
+++ include/game/force.hpp
@@ -13,6 +13,38 @@ namespace game
       m_LifeTime = lifeTime;
     }
 
+    Force(const Force& cpy)
+    {
+      m_Force = cpy.m_Force;
+      m_Dir = cpy.m_Dir;
+      m_LifeTime = cpy.m_LifeTime;
+    }
+    
+    Force& operator=(const Force& asgn)
+    {
+      m_Force = asgn.m_Force;
+      m_Dir = asgn.m_Dir;
+      m_LifeTime = asgn.m_LifeTime;
+      return *this;
+    }
+    
+    Force(const Force&& rhs)
+    {
+      m_Force = std::move(rhs.m_Force);
+      m_Dir = std::move(rhs.m_Dir);
+      m_LifeTime = rhs.m_LifeTime;
+    }
+    
+    Force& operator=(const Force&& rhs)
+    {
+      m_Force = std::move(rhs.m_Force);
+      m_Dir = std::move(rhs.m_Dir);
+      m_LifeTime = rhs.m_LifeTime;
+      return *this;
+    }
+    
+    
+    
     float m_LifeTime;
     game::vec2<float> m_Force;
     game::vec2<float> m_Dir;
